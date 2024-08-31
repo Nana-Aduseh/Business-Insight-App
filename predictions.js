e460ff1a8fda6f6b5e47a1e7b8ad59fed0c11da5
Nana-Aduseh <128828209+Nana-Aduseh@users.noreply.github.com>
2024-08-31 01:48:23 +0000

    Add files via upload

--- predictions.js
+++ predictions.js
@@ -1,82 +1,229 @@
-import { StyleSheet, Text, View, Image, ScrollView,TouchableOpacity } from 'react-native'
-import React from 'react'
-import Icon from 'react-native-vector-icons/FontAwesome'
+import { StyleSheet, Text, View, ScrollView, TouchableOpacity } from 'react-native';
+import React from 'react';
+import Icon from 'react-native-vector-icons/FontAwesome';
 import { useNavigation } from '@react-navigation/native';
 
 const App = () => {
-    const navigation = useNavigation();
-  
-    return (
-      <View>
-        <View style={styles.header}></View>
+  const navigation = useNavigation();
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.header}>
         <TouchableOpacity onPress={() => navigation.goBack()} style={styles.backButton}>
-          <Icon name='angle-double-left' style={{ marginLeft: '3%'}} size={30}/>
+          <Icon name='angle-double-left' size={30} color="#ffffff"/>
         </TouchableOpacity>
-      <Text style={styles.title}>Predictions</Text>
-      <ScrollView>
-      <Text style={styles.subtitle}>Continued Dominance of Fridays: </Text>
-      <Text style={styles.paragraph}>Given that Fridays consistently emerge as the busiest and 
-      most profitable days, it is anticipated that this trend will persist. Factors such as the end of the workweek and 
-      increased social activities may contribute to sustained high demand on Fridays</Text>
+        <Text style={styles.title}>Predictions</Text>
+      </View>
+      
+      <ScrollView style={styles.scrollView}>
+        <Text style={styles.subtitle}>Continued Dominance of Fridays:</Text>
+        <Text style={styles.paragraph}>Given that Fridays consistently emerge as the busiest and most profitable days, it is anticipated that this trend will persist. Factors such as the end of the workweek and increased social activities may contribute to sustained high demand on Fridays.</Text>
+        
+        <Text style={styles.subtitle}>Steady Sales Growth on Saturdays:</Text>
+        <Text style={styles.paragraph}>Saturdays closely follow Fridays in terms of order quantity, suggesting a strong consumer demand during the weekend. It is predicted that sales on Saturdays will continue to grow steadily, possibly rivaling or even surpassing those of Fridays in the future.</Text>
 
-      <Text style={styles.subtitle}>Steady Sales Growth on Saturdays: </Text>
-      <Text style={styles.paragraph}> Saturdays closely follow Fridays in terms of order quantity, 
-      suggesting a strong consumer demand during the weekend. It is predicted that sales on Saturdays will continue to grow
-       steadily, possibly rivaling or even surpassing those of Fridays in the future.</Text>
+        <Text style={styles.subtitle}>Potential Sunday Sales Optimization:</Text>
+        <Text style={styles.paragraph}>With Sundays consistently exhibiting lower activity and profitability, there is an opportunity for strategic initiatives aimed at boosting sales on this day. Introducing special promotions or marketing campaigns tailored to Sunday customers may help optimize sales and improve profitability.</Text>
 
-       <Text style={styles.subtitle}>Potential Sunday Sales Optimization:</Text>
-      <Text style={styles.paragraph}>With Sundays consistently exhibiting lower activity and 
-      profitability, there is an opportunity for strategic initiatives aimed at boosting sales on this day. Introducing special
-       promotions or marketing campaigns tailored to Sunday customers may help optimize sales and improve profitability.</Text>
-
-       <Text style={styles.subtitle}>Persistent Popularity of Large Pizza Sizes:</Text>
-      <Text style={styles.paragraph}> The dominance of Large pizza sizes in sales indicates
-       a strong preference among customers. It is forecasted that Large pizzas will continue to be the preferred choice,
-        potentially prompting further exploration of product offerings in this size category</Text>
+        <Text style={styles.subtitle}>Persistent Popularity of Large Pizza Sizes:</Text>
+        <Text style={styles.paragraph}>The dominance of Large pizza sizes in sales indicates a strong preference among customers. It is forecasted that Large pizzas will continue to be the preferred choice, potentially prompting further exploration of product offerings in this size category.</Text>
 
         <Text style={styles.subtitle}>Varied Performance of Pizza Varieties:</Text>
-      <Text style={styles.paragraph}> The performance of different pizza varieties highlights 
-      consumer preferences and trends. Pizzas such as the Classic Deluxe and Thai Chicken consistently perform well, 
-      suggesting continued popularity. Conversely, pizzas like the Brie Carre may require reassessment or refinement to improve
-       sales performance.</Text>
+        <Text style={styles.paragraph}>The performance of different pizza varieties highlights consumer preferences and trends. Pizzas such as the Classic Deluxe and Thai Chicken consistently perform well, suggesting continued popularity. Conversely, pizzas like the Brie Carre may require reassessment or refinement to improve sales performance.</Text>
 
-       <Text style={styles.subtitle}>Lunchtime Surge in Orders:</Text>
-      <Text style={styles.paragraph}>  The peak in order activity around noon, particularly on most days,
-       indicates a strong correlation with lunchtime.It is predicted that this trend will persist, with lunchtime continuing to be
-        a key period for high order volumes.Businesses may leverage this insight to optimize staffing and operational efficiency 
-        during peak hours.</Text>
-        <View style={{height:150}}></View>
+        <Text style={styles.subtitle}>Lunchtime Surge in Orders:</Text>
+        <Text style={styles.paragraph}>The peak in order activity around noon, particularly on most days, indicates a strong correlation with lunchtime. It is predicted that this trend will persist, with lunchtime continuing to be a key period for high order volumes. Businesses may leverage this insight to optimize staffing and operational efficiency during peak hours.</Text>
+        
+        <View style={{height: 50}}></View>
       </ScrollView>
     </View>
-  )
-}
+  );
+};
 
-export default App
+export default App;
 
 const styles = StyleSheet.create({
-  header:{
-    width:'100%',
-    height:'5%',
-    color: 'white',
+  container: {
+    flex: 1,
+    backgroundColor: '#f5f5f5',
   },
-  title:{
-    fontWeight:'bold',
-    fontSize:30,
-    marginLeft:'5%',
-    marginBottom:'2%',
-    textDecorationLine: 'underline'
+  header: {
+    width: '100%',
+    height: 80,
+    backgroundColor: '#4A99C6',
+    flexDirection: 'row',
+    alignItems: 'center',
+    paddingHorizontal: 10,
   },
-  subtitle:{
-    fontWeight:'bold',
-    fontSize:20,
-    marginLeft:'5%',
+  title: {
+    color: '#ffffff',
+    fontWeight: 'bold',
+    fontSize: 24,
+    marginLeft: 10,
+    marginTop:30
   },
-  paragraph:{
-    fontSize:18,
-    marginLeft:'8%',
-    marginRight:'7%'
+  scrollView: {
+    paddingHorizontal: 15,
+    paddingTop: 10,
+  },
+  subtitle: {
+    fontWeight: 'bold',
+    fontSize: 20,
+    marginTop: 15,
+    marginBottom: 5,
+    color: '#333333',
+  },
+  paragraph: {
+    fontSize: 16,
+    lineHeight: 22,
+    color: '#666666',
+    marginBottom: 10,
   },
   backButton: {
-    marginTop:'2%'
-  }
-})
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop:30
+  },
+});
+
+// import { StyleSheet, Text, View, ScrollView, TouchableOpacity } from 'react-native';
+// import React, { useState } from 'react';
+// import Icon from 'react-native-vector-icons/FontAwesome';
+// import { useNavigation } from '@react-navigation/native';
+
+// const App = () => {
+//   const navigation = useNavigation();
+//   const [activeSection, setActiveSection] = useState(null);
+
+//   const toggleSection = (section) => {
+//     setActiveSection(activeSection === section ? null : section);
+//   };
+
+//   return (
+//     <View style={styles.container}>
+//       <View style={styles.header}>
+//         <TouchableOpacity onPress={() => navigation.goBack()} style={styles.backButton}>
+//           <Icon name='angle-double-left' size={30} color="#ffffff" />
+//         </TouchableOpacity>
+//         <Text style={styles.title}>Predictions</Text>
+//       </View>
+
+//       <ScrollView style={styles.scrollView}>
+//         <TouchableOpacity onPress={() => toggleSection('section1')} style={styles.tab}>
+//           <Text style={styles.subtitle}>Continued Dominance of Fridays</Text>
+//           <Icon name={activeSection === 'section1' ? 'angle-up' : 'angle-down'} size={20} color="#333" />
+//         </TouchableOpacity>
+//         {activeSection === 'section1' && (
+//           <Text style={styles.paragraph}>
+//             Given that Fridays consistently emerge as the busiest and most profitable days, it is anticipated that this trend will persist. Factors such as the end of the workweek and increased social activities may contribute to sustained high demand on Fridays.
+//           </Text>
+//         )}
+
+//         <TouchableOpacity onPress={() => toggleSection('section2')} style={styles.tab}>
+//           <Text style={styles.subtitle}>Steady Sales Growth on Saturdays</Text>
+//           <Icon name={activeSection === 'section2' ? 'angle-up' : 'angle-down'} size={20} color="#333" />
+//         </TouchableOpacity>
+//         {activeSection === 'section2' && (
+//           <Text style={styles.paragraph}>
+//             Saturdays closely follow Fridays in terms of order quantity, suggesting a strong consumer demand during the weekend. It is predicted that sales on Saturdays will continue to grow steadily, possibly rivaling or even surpassing those of Fridays in the future.
+//           </Text>
+//         )}
+
+//         <TouchableOpacity onPress={() => toggleSection('section3')} style={styles.tab}>
+//           <Text style={styles.subtitle}>Potential Sunday Sales Optimization</Text>
+//           <Icon name={activeSection === 'section3' ? 'angle-up' : 'angle-down'} size={20} color="#333" />
+//         </TouchableOpacity>
+//         {activeSection === 'section3' && (
+//           <Text style={styles.paragraph}>
+//             With Sundays consistently exhibiting lower activity and profitability, there is an opportunity for strategic initiatives aimed at boosting sales on this day. Introducing special promotions or marketing campaigns tailored to Sunday customers may help optimize sales and improve profitability.
+//           </Text>
+//         )}
+
+//         <TouchableOpacity onPress={() => toggleSection('section4')} style={styles.tab}>
+//           <Text style={styles.subtitle}>Persistent Popularity of Large Pizza Sizes</Text>
+//           <Icon name={activeSection === 'section4' ? 'angle-up' : 'angle-down'} size={20} color="#333" />
+//         </TouchableOpacity>
+//         {activeSection === 'section4' && (
+//           <Text style={styles.paragraph}>
+//             The dominance of Large pizza sizes in sales indicates a strong preference among customers. It is forecasted that Large pizzas will continue to be the preferred choice, potentially prompting further exploration of product offerings in this size category.
+//           </Text>
+//         )}
+
+//         <TouchableOpacity onPress={() => toggleSection('section5')} style={styles.tab}>
+//           <Text style={styles.subtitle}>Varied Performance of Pizza Varieties</Text>
+//           <Icon name={activeSection === 'section5' ? 'angle-up' : 'angle-down'} size={20} color="#333" />
+//         </TouchableOpacity>
+//         {activeSection === 'section5' && (
+//           <Text style={styles.paragraph}>
+//             The performance of different pizza varieties highlights consumer preferences and trends. Pizzas such as the Classic Deluxe and Thai Chicken consistently perform well, suggesting continued popularity. Conversely, pizzas like the Brie Carre may require reassessment or refinement to improve sales performance.
+//           </Text>
+//         )}
+
+//         <TouchableOpacity onPress={() => toggleSection('section6')} style={styles.tab}>
+//           <Text style={styles.subtitle}>Lunchtime Surge in Orders</Text>
+//           <Icon name={activeSection === 'section6' ? 'angle-up' : 'angle-down'} size={20} color="#333" />
+//         </TouchableOpacity>
+//         {activeSection === 'section6' && (
+//           <Text style={styles.paragraph}>
+//             The peak in order activity around noon, particularly on most days, indicates a strong correlation with lunchtime. It is predicted that this trend will persist, with lunchtime continuing to be a key period for high order volumes. Businesses may leverage this insight to optimize staffing and operational efficiency during peak hours.
+//           </Text>
+//         )}
+
+//         <View style={{ height: 150 }}></View>
+//       </ScrollView>
+//     </View>
+//   );
+// };
+
+// export default App;
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#f5f5f5',
+//   },
+//   header: {
+//     width: '100%',
+//     height: 60,
+//     backgroundColor: '#007BFF',
+//     flexDirection: 'row',
+//     alignItems: 'center',
+//     paddingHorizontal: 10,
+//   },
+//   title: {
+//     color: '#ffffff',
+//     fontWeight: 'bold',
+//     fontSize: 24,
+//     marginLeft: 10,
+//   },
+//   scrollView: {
+//     paddingHorizontal: 15,
+//     paddingTop: 10,
+//   },
+//   tab: {
+//     flexDirection: 'row',
+//     justifyContent: 'space-between',
+//     alignItems: 'center',
+//     paddingVertical: 10,
+//     paddingHorizontal: 5,
+//     backgroundColor: '#e0e0e0',
+//     marginVertical: 5,
+//     borderRadius: 5,
+//   },
+//   subtitle: {
+//     fontWeight: 'bold',
+//     fontSize: 18,
+//     color: '#333333',
+//   },
+//   paragraph: {
+//     fontSize: 16,
+//     lineHeight: 22,
+//     color: '#666666',
+//     marginBottom: 10,
+//     paddingHorizontal: 10,
+//   },
+//   backButton: {
+//     justifyContent: 'center',
+//     alignItems: 'center',
+//   },
+// });
--- report.js
+++ report.js
@@ -1,81 +1,87 @@
-import { StyleSheet, Text, View, Image, ScrollView ,TouchableOpacity } from 'react-native'
-import React from 'react'
-import Icon from 'react-native-vector-icons/FontAwesome'
+import { StyleSheet, Text, View, ScrollView, TouchableOpacity } from 'react-native';
+import React from 'react';
+import Icon from 'react-native-vector-icons/FontAwesome';
 import { useNavigation } from '@react-navigation/native';
 
 const App = () => {
   const navigation = useNavigation();
 
   return (
-    <View>
-      <View style={styles.header}></View>
-      <TouchableOpacity onPress={() => navigation.goBack()} style={styles.backButton}>
-        <Icon name='angle-double-left' style={{ marginLeft: '3%'}} size={30}/>
-      </TouchableOpacity>
-      <Text style={styles.title}>Reports</Text>
-      <ScrollView Vertical>
-       <Text style={styles.subtitle}>Busiest Day;</Text>
-       <Text style={styles.paragraph}>Friday stands out as the busiest day, recording a total of 8242 orders.
-        Following closely behind is Saturday, with 7493 orders. Conversely, Sundays exhibit the lowest activity,
-         with only 6035 orders.</Text>
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <TouchableOpacity onPress={() => navigation.goBack()} style={styles.backButton}>
+          <Icon name='angle-double-left' size={30} color="#ffffff"/>
+        </TouchableOpacity>
+        <Text style={styles.title}>Reports</Text>
+      </View>
       
-      <Text style={styles.subtitle}>Most Profitable Day;</Text>
-      <Text style={styles.paragraph}> Fridays also emerge as the most profitable days, generating sales worth $136,073.90,
-        constituting 16.64% of total sales. Thursdays follow suit with sales totaling $412,528.50 (15.10%).
-         In contrast, Sundays yield the least profit, with sales amounting to $99,203.50, contributing to 12.13% of total
-          sales.</Text>
+      <ScrollView style={styles.scrollView}>
+        <Text style={styles.subtitle}>Busiest Day:</Text>
+        <Text style={styles.paragraph}>Friday stands out as the busiest day, recording a total of 8242 orders. Following closely behind is Saturday, with 7493 orders. Conversely, Sundays exhibit the lowest activity, with only 6035 orders.</Text>
+        
+        <Text style={styles.subtitle}>Most Profitable Day:</Text>
+        <Text style={styles.paragraph}>Fridays also emerge as the most profitable days, generating sales worth $136,073.90, constituting 16.64% of total sales. Thursdays follow suit with sales totaling $412,528.50 (15.10%). In contrast, Sundays yield the least profit, with sales amounting to $99,203.50, contributing to 12.13% of total sales.</Text>
 
-      <Text style={styles.subtitle}>Best & Worst Selling Pizza Size;</Text>
-      <Text style={styles.paragraph}>The Large size pizza dominates sales, with 18,956 orders, closely trailed by the Medium 
-       size with 15,635 orders. In contrast, the Extra Extra Large size registers the lowest demand, with only 28 orders.</Text>
+        <Text style={styles.subtitle}>Best & Worst Selling Pizza Size:</Text>
+        <Text style={styles.paragraph}>The Large size pizza dominates sales, with 18,956 orders, closely trailed by the Medium size with 15,635 orders. In contrast, the Extra Extra Large size registers the lowest demand, with only 28 orders.</Text>
 
-      <Text style={styles.subtitle}>Best and Worst selling Pizzas;</Text>
-      <Text style={styles.paragraph}>The Classic Deluxe Pizza leads as the best-selling option, with 2453 orders, closely
-      followed by The Barbecue Chicken Pizza with 2432 orders. Conversely, The Brie Carre Pizza ranks as the least popular,
-        with minimal sales</Text>
+        <Text style={styles.subtitle}>Best and Worst Selling Pizzas:</Text>
+        <Text style={styles.paragraph}>The Classic Deluxe Pizza leads as the best-selling option, with 2453 orders, closely followed by The Barbecue Chicken Pizza with 2432 orders. Conversely, The Brie Carre Pizza ranks as the least popular, with minimal sales.</Text>
 
-      <Text style={styles.subtitle}>Most and Least Profitable Pizzas;</Text>
-      <Text style={styles.paragraph}>The Thai Chicken Pizza proves to be the most profitable, generating sales of $43,434.25, 
-      constituting 5.31% of total sales. Following closely behind is The Barbecue Chicken Pizza, with sales totaling $42,763.00
-       (5.23%). In contrast, The Brie Carre Pizza yields the lowest profit margins.</Text>  
+        <Text style={styles.subtitle}>Most and Least Profitable Pizzas:</Text>
+        <Text style={styles.paragraph}>The Thai Chicken Pizza proves to be the most profitable, generating sales of $43,434.25, constituting 5.31% of total sales. Following closely behind is The Barbecue Chicken Pizza, with sales totaling $42,763.00 (5.23%). In contrast, The Brie Carre Pizza yields the lowest profit margins.</Text>  
 
-      <Text style={styles.subtitle}>Busiest Hour:</Text>
-      <Text style={styles.paragraph}>
-      The hour around noon, specifically 12:00 PM, experiences the highest activity consistently across most days.
-       This peak coincides with lunchtime, indicating a surge in orders during this period.Conversely, the least busy hour typically 
-       falls around 9:00 AM.</Text>  
-      <View style={{height:150}}></View>
+        <Text style={styles.subtitle}>Busiest Hour:</Text>
+        <Text style={styles.paragraph}>The hour around noon, specifically 12:00 PM, experiences the highest activity consistently across most days. This peak coincides with lunchtime, indicating a surge in orders during this period. Conversely, the least busy hour typically falls around 9:00 AM.</Text>
+        
+        <View style={{height: 50}}></View>
       </ScrollView>
     </View>
-  )
-}
+  );
+};
 
-export default App
+export default App;
 
 const styles = StyleSheet.create({
-  header:{
-    width:'100%',
-    height:'5%',
-    color: 'white',
+  container: {
+    flex: 1,
+    backgroundColor: '#f5f5f5',
   },
-  title:{
-    fontWeight:'bold',
-    fontSize:30,
-    marginLeft:'5%',
-    marginBottom:'2%',
-    textDecorationLine: 'underline',
+  header: {
+    width: '100%',
+    height: 80,
+    backgroundColor: '#4A99C6',
+    flexDirection: 'row',
+    alignItems: 'center',
+    paddingHorizontal: 10,
   },
-  subtitle:{
-    fontWeight:'bold',
-    fontSize:20,
-    marginLeft:'5%',
+  title: {
+    color: '#ffffff',
+    fontWeight: 'bold',
+    fontSize: 24,
+    marginLeft: 10,
+    marginTop:30,
   },
-  paragraph:{
-    fontSize:18,
-    marginLeft:'8%',
-    marginRight:'7%'
+  scrollView: {
+    paddingHorizontal: 15,
+    paddingTop: 10,
+  },
+  subtitle: {
+    fontWeight: 'bold',
+    fontSize: 20,
+    marginTop: 15,
+    marginBottom: 5,
+    color: '#333333',
+  },
+  paragraph: {
+    fontSize: 16,
+    lineHeight: 22,
+    color: '#666666',
+    marginBottom: 10,
   },
   backButton: {
-    marginTop:'2%'
-  }
-})
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop:30,
+  },
+});
